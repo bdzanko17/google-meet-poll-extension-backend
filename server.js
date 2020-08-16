32ccf910b07dc97ac946d81f4056476be3bd7bfc
Bdzanko17 <bdzanko@gmail.com>
2020-08-16 11:30:14 +0200

    poll-changes

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const server = require("http").createServer();
 const io = require("socket.io")(server);
 
+var ansvers={"1":0,"2":0,"3":0}
+
 io.on('connect', socket => {
   socket.on("vote",data=>{
+    socket.broadcast.emit("active_poll",data);
     socket.emit("active_poll",data);
+    votes=0;
+    ansvers={"1":0,"2":0,"3":0};
+    setTimeout(() => {
+      socket.broadcast.emit("results",ansvers);
+      socket.emit("results",ansvers);
+    }, 20000);
   });
 
   socket.on("votes",data=>{
-    console.log(data);
+    ansvers[data]++;
   });
-  
+
 });
 
 server.listen(3000);

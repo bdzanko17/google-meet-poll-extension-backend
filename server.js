2cde8afa9b74d0b3b38d4cee7387bea143bdab5e
bdzanko17 <bdzanko1@gmail.com>
2020-08-18 11:13:47 +0200

    Deploying server side

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const server = require("http").createServer();
+const express= require("express");
+const http = require("http");
 
-var io = require("socket.io")(server, {
-  handlePreflightRequest: (req, res) => {
-      const headers = {
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-          "Access-Control-Allow-Credentials": true
-      };
-      res.writeHead(200, headers);
-      res.end();
-  }
+const app = express();
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
 });
+
 var ansvers={"1":0,"2":0,"3":0}
 
 io.on('connect', socket => {

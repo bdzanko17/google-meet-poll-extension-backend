ccecffd6a0da581d3ccc1bc4c3401bf1cb68952d
bdzanko17 <bdzanko1@gmail.com>
2020-08-18 11:06:39 +0200

    changes

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const server = require("http").createServer();
-var io = require('socket.io')(server, { origins: '*:*'});
 
+var io = require("socket.io")(server, {
+  handlePreflightRequest: (req, res) => {
+      const headers = {
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+          "Access-Control-Allow-Credentials": true
+      };
+      res.writeHead(200, headers);
+      res.end();
+  }
+});
 var ansvers={"1":0,"2":0,"3":0}
 
 io.on('connect', socket => {

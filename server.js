3e71212d32cc2d138bee775155003bc873cd4535
bdzanko17 <bdzanko1@gmail.com>
2020-09-20 19:05:57 +0200

    Mapping results

--- server.js
+++ server.js
@@ -4,38 +4,32 @@ var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 var cors = require("cors");
 var code = 12345;
-var ansvers = { 1: 0, 2: 0, 3: 0 };
-
+var answers = new Map();
 app.use(cors());
 
 io.on("connect", (socket) => {
-
   socket.on("join", (room) => {
     socket.join(room);
     console.log("Connected to room " + room);
   });
 
-
-  socket.on("vote", ({ room, data,code }) => {
-    console.log("SOBA JE " + room + "data is " + data);
+  socket.on("vote", ({ room, data, code }) => {
+    ansvers = { 1: 0, 2: 0, 3: 0 };
+    answers.set(room, ansvers);
     socket.broadcast.to(room).emit("active_poll", data);
     socket.emit("active_poll", data);
-    votes = 0;
-    ansvers = { 1: 0, 2: 0, 3: 0 };
     setTimeout(() => {
-      socket.broadcast.to(room).emit("results", ansvers);
-      socket .emit("results", ansvers);
+      socket.broadcast.to(room).emit("results",  answers.get(room));
+      socket.emit("results", answers.get(room));
     }, 20000);
   });
 
-  socket.on("code",(codee)=>{
-    if(code=codee)
-    socket.emit("organizator",1);
-  })
+  socket.on("code", (codee) => {
+    if ((code = codee)) socket.emit("organizator", 1);
+  });
 
-  socket.on("votes", ( answer ) => {
-    console.log(answer);
-    ansvers[answer]++;
+  socket.on("votes", ({ answer, room }) => {
+    answers.get(room)[answer]++;
   });
 });
 

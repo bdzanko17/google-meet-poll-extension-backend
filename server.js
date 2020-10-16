fcc3e2db76033787ace8a4fb33a942504aeaf2bb
bdzanko17 <bdzanko1@gmail.com>
2020-10-16 09:55:03 +0200

    Answer counter

--- server.js
+++ server.js
@@ -5,24 +5,37 @@ var io = require("socket.io").listen(server);
 var cors = require("cors");
 var answers = new Map();
 var codes = new Map();
+var active = new Map();
+var socket_rooms = new Map();
+var active_answers = new Map();
+
 app.use(cors());
 
 io.on("connect", (socket) => {
   socket.on("join", (room) => {
     socket.join(room);
-    if(codes.get(room)==null)
-    codes.set(room,room.slice(24,27))
-    console.log("Connected to room " + room);
+    socket_rooms.set(socket.id,room);
+    if(codes.get(room)==null){
+      codes.set(room,room.slice(24,27))
+      active.set(room,1);
+      console.log("Connected to room " + room);
+    }else{
+      let x = active.get(room);
+      x++
+      active.set(room,x)
+    }
+    if(active_answers.get(room)==null){
+      active_answers.set(room,0);
+    }
   });
   socket.on("vote", ({ room, data, code }) => {
+    
     ansvers = { 1: 0, 2: 0, 3: 0 };
     answers.set(room, ansvers);
     socket.broadcast.to(room).emit("active_poll", data);
     socket.emit("active_poll", data);
-    setTimeout(() => {
-      socket.broadcast.to(room).emit("results",  answers.get(room));
-      socket.emit("results", answers.get(room));
-    }, 20000);
+    active_answers.set(room,0);
+    
   });
 
   socket.on("code", (codee,room) => {
@@ -31,8 +44,24 @@ io.on("connect", (socket) => {
     socket.emit("organizator",0)
   });
 
+  socket.on("disconnect",function(){
+    let room = socket_rooms.get(socket.id);
+    let x = active.get(room)
+    x--;
+    active.set(room,x);
+    console.log(active.get(room))
+  });
+
   socket.on("votes", ({ answer, room }) => {
     answers.get(room)[answer]++;
+    var x = active_answers.get(room);
+    x++;
+    active_answers.set(room,x);
+  
+    if(active_answers.get(room)==active.get(room)){
+      socket.broadcast.to(room).emit("results",  answers.get(room));
+      socket.emit("results", answers.get(room));
+    }
   });
 });
 
